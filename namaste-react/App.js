faef481217470a5cc4eeacd0407d216942944911
Deepanshu Mainh <dmainh28@gmail.com>
2024-10-27 12:05:14 +0530

    namaste react projects

--- namaste-react/App.js
+++ namaste-react/App.js
@@ -1,26 +1,37 @@
-// ReactElement(Object) => HTML(Browser Understands)
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-]);
+// React.createElement => ReactElement-JS Object =>HTMLElement(render)
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from React!"
+/* const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+console.log(heading); */
+
+// JSX - HTML-like or XML-like syntax
+// JSX (transpiled before it reaches the JS Engine) - PARCEL - Babel
+// JSX => Babels transpile it to React.createElement => ReactElement-JS Object =>HTMLElement(render)
+
+const elem = <span>React Element</span>;
+
+const Title = () => (
+  <h1 id="heading" className="head" tabIndex="5">
+    Namaste React using JSX 🚀
+  </h1>
 );
 
-console.log(parent); // object
+// React Component
+// Class based component - OLD
+// functional component - New
 
-//JSX
+// React Functional Component
+// Component Composition
+const HeadingComponent = () => (
+  <div id="container">
+    {Title()}
+    <h1 className="heading">Namaste React Functional Component</h1>
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+root.render(<HeadingComponent />);
--- namaste-react/README.md
+++ namaste-react/README.md
@@ -1,1 +1,21 @@
 # Namaste React
+
+# Parcel
+
+- dev build
+- local server
+- hmr = hot module replacement
+- file watching algorithm - written in c++
+- caching - faster builds
+- image optimization
+- minification
+- bundling
+- compress
+- consistent hashing
+- code splitting
+- differential bundling - support older browsers
+- diagnostic
+- error handling
+- https
+- tree shaking - remove unused code
+- different dev and prod bundles
